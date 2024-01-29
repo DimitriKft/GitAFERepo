1a442630d017b2967ff10d482fcab461379e5122
Dimitri Klopfstein <dimitri.kft@gmail.com>
2024-01-29 16:03:06 +0100

    merge dev master

---
+++ GitAFE/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  GitAFE
+//
+//  Created by dimitri on 29/01/2024.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("Nouvelle vue !")
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
